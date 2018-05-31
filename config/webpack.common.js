7ba4cfe2584982cdb5edc685139e49d4734b8f06
Jhan Mateo <nino.jhan.n.mateo>
2018-06-01 03:33:09 +0800

    fixed missing file

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,4 +1,3 @@
-const framework = require('./framework');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
